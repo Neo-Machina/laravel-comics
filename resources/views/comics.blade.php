5fba049cc222bf1cc123aac0d38c4cf4b7fcaeff
Neo-Machina <andreistatache1997@gmail.com>
2022-08-24 16:58:14 +0200

    layout and basic pages creation

---
+++ resources/views/comics.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('page_title')
+    Comics
+@endsection
+
+@section('main_content')
+    <h1>comics</h1>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('page_title')
+    Homepage
+@endsection
+
+@section('main_content')
+    <h1>homepage</h1>
+@endsection
