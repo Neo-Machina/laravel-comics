62851259485b03c2a80c69049b0ee7443e9ae42e
Neo-Machina <andreistatache1997@gmail.com>
2022-08-25 01:11:39 +0200

    header completed

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,83 @@
 <header>
-    <h2>header</h2>
+    {{-- Header-top --}}
+    <div class="header-top">
+        <div class="container flex-header-top">
+            <div>
+                dc power visa&reg;
+            </div>
+            <div>
+                addition dc sites <span><i class="fa-solid fa-caret-down"></i></span>
+            </div>
+        </div>
+    </div>
+
+    {{-- Header-bottom --}}
+    <div class="container">
+        <div class="flex-header-bottom">
+            <!-- header-logo -->
+            <div class="logo">
+                <img src="{{ asset('img/dc-logo.png') }}" alt="logo-dc">
+            </div>
+    
+            <!-- header-nav -->
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">
+                            characters
+                        </a>
+                    </li>   
+                    <li class="selected-item">
+                        <a href="#">
+                            comics
+                        </a>
+                    </li>  
+                    <li>
+                        <a href="#">
+                            movies
+                        </a>
+                    </li>  
+                    <li>
+                        <a href="#">
+                            tv
+                        </a>
+                    </li>  
+                    <li>
+                        <a href="#">
+                            games
+                        </a>
+                    </li>  
+                    <li>
+                        <a href="#">
+                            collectibles
+                        </a>
+                    </li>  
+                    <li>
+                        <a href="#">
+                            videos
+                        </a>
+                    </li>    
+                    <li>
+                        <a href="#">
+                            fans
+                        </a>
+                    </li>  
+                    <li>
+                        <a href="#">
+                            news
+                        </a>
+                    </li>  
+                    <li>
+                        <a href="#">
+                            shop <span><i class="fa-solid fa-caret-down"></i></span>
+                        </a>
+                    </li>  
+                    <li>
+                        {{-- search-bar --}}
+                        <input type="text" placeholder="Search"> <i class="fa-solid fa-magnifying-glass"></i>
+                    </li>     
+                </ul>
+            </nav>
+        </div>
+    </div>
 </header>
