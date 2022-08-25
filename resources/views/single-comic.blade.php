b7820aa9b4889c579f27eff1834fc51943178012
Neo-Machina <andreistatache1997@gmail.com>
2022-08-25 23:45:26 +0200

    comic-section-description done

--- resources/views/single-comic.blade.php
+++ resources/views/single-comic.blade.php
@@ -7,8 +7,8 @@
 @section('main_content')
     @include('components.jumbotron')
 
-    {{-- comic-book-cover --}}
-    <div class="comic-book-section">
+    {{-- comic-book-cover-section --}}
+    <section class="comic-book-section">
         <div class="small_container">
             <div class="comic-cover-card">
                 <img src="{{ $current_comic['thumb'] }}" alt="{{ $current_comic['series'] }}">
@@ -22,5 +22,41 @@
                 </div>
             </div>
         </div>
-    </div>
+    </section>
+
+    {{-- comic-description-section --}}
+    <section id="comic-description-section">
+        <div class="small_container">
+            <div class="comic-row">
+                <div class="comic-left-col">
+                    <h1>{{ $current_comic['title'] }}</h1>
+
+                    <div class="row-price">
+                        <div class="left-col-price">
+                            <div class="us-price">
+                                <span class="green_text">U.S. Price:</span> $19.99
+                            </div>
+                            <div class="status_available">
+                                <span class="green_text">available</span>
+                            </div>
+                        </div>
+                        <div class="right-col-price">
+                            check availability <span><i class="fa-solid fa-caret-down"></i></span>
+                        </div>
+                    </div>
+
+                    <div class="text_description">
+                        <p>
+                            {{ $current_comic['description'] }}
+                        </p>
+                    </div>
+                </div>
+                <div class="comic-right-col">
+                    <div>advertisment</div>
+
+                    <img src="{{ asset('img/adv.jpg') }}" alt="advertisment">
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
